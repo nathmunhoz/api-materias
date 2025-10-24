fec78fce3255df2f3fe6c764877eda42483e51e6
NATHALIA BATISTA MUNHOZ (2501617) <2501617@edu.anchieta.br>
2025-10-23 21:14:09 -0300

    finalizado api de materia integrado com banco de dados

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# api-materias
+# Projeto de Cadastro de Alunos (Node.js + PostgreSQL + TypeScript)
+
+Este projeto tem como objetivo realizar o **cadastro de alunos** e suas **notas** em um banco de dados **PostgreSQL**, calculando automaticamente as médias de cada matéria antes de salvar.
+
+- Breno Miguel de Souza Afonso - RA: 2500998
+- Nathalia Batista Munhoz - RA: 2501617
+- Vanessa da Silva Santos - RA: 2520743
+
+---
+
+## Tecnologias Utilizadas
+
+- **Node.js** – Ambiente de execução JavaScript  
+- **PostgreSQL** – Banco de dados relacional  
+- **TypeScript** – Superset do JavaScript com tipagem estática  
+- **readline-sync** – Entrada de dados via terminal  
+- **pg** – Cliente PostgreSQL para Node.js  
+
+---
+
+## Configuração do Ambiente
+
+### 1. Instalar o Node.js
+Baixe e instale o Node.js em:
+[https://nodejs.org](https://nodejs.org)
+
+Verifique se está instalado:
+```bash
+node -v
+npm -v
+```
+
+Criar e acessar a pasta do projeto
+```bash
+git clone https://github.com/nathmunhoz/api-materias.git
+cd api-materias
+code .
+```
+
+Iniciar o projeto
+```bash
+npm init -y
+```
+
+Instalar dependências
+```bash
+npm install pg readline-sync
+npm install -D typescript @types/pg @types/readline-sync
+```
+
+Criar a configuração do TypeScript
+```bash
+npx tsc --init
+
+```
+
+---
+## Configuração do Banco de Dados
+
+![banco](image.png)
+
+
+
+---
+## Executando o Projeto
+
+Compile o TypeScript e Execute:
+```bash
+npx tsc
+node dist/exercicio.js
+```
+
+---
+## Resultado Esperado
+```bash
+--- Cadastro de Aluno ---
+Digite o nome: Ana
+Digite a série: 8º Ano
+Digite a idade: 13
+Digite a nota 1 de matematica: 8
+...
+-----------------------------------------
+Dados inseridos com sucesso!
+Nome: Ana, Idade: 13, Série: 8º Ano, Matemática: 8.25, Geografia: 7.87, História: 9.12
+-----------------------------------------
+Conexão com o banco de dados encerrada.
+```
+![vs code](image-1.png)
---
+++ dist/exercicio.js
@@ -0,0 +1,68 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { Pool } = require('pg');
+const readlineSync = require('readline-sync');
+const dbConfig = {
+    user: 'aluno',
+    host: 'localhost',
+    database: 'db_profedu',
+    password: '102030',
+    port: 5432,
+};
+const pool = new Pool(dbConfig);
+async function inserirDados() {
+    console.log("--- Cadastro de Aluno ---");
+    const nome = readlineSync.question('Digite o nome: ');
+    const serie = readlineSync.question('Digite a série: ');
+    const idade = readlineSync.questionInt('Digite a idade: ');
+    let matematica = [];
+    for (let i = 1; i <= 8; i++) {
+        let nota = readlineSync.questionFloat(`Digite a nota ${i} de matematica: `);
+        matematica.push(nota);
+    }
+    let geografia = [];
+    for (let i = 1; i <= 8; i++) {
+        let nota = readlineSync.questionFloat(`Digite a nota ${i} de geografia: `);
+        geografia.push(nota);
+    }
+    let historia = [];
+    for (let i = 1; i <= 8; i++) {
+        let nota = readlineSync.questionFloat(`Digite a nota ${i} de história: `);
+        historia.push(nota);
+    }
+    const somaM = matematica.reduce((total, n) => total + n, 0);
+    const mediaM = somaM / matematica.length;
+    const somaG = geografia.reduce((total, n) => total + n, 0);
+    const mediaG = somaG / geografia.length;
+    const somaH = historia.reduce((total, n) => total + n, 0);
+    const mediaH = somaH / historia.length;
+    if (!nome || !idade || !serie || !historia || !geografia || !matematica) {
+        console.error("Erro: Todos os campos são obrigatórios! Operação cancelada.");
+        await pool.end();
+        return;
+    }
+    try {
+        console.log("\nConectando ao banco de dados...");
+        const client = await pool.connect();
+        console.log("Conexão bem-sucedida! Inserindo dados...");
+        const insertQuery = `
+            INSERT INTO public.alunos (nome, idade, serie, matematica, geografia, historia)
+            VALUES ($1, $2, $3, $4, $5, $6)
+        `;
+        const values = [nome, idade, serie, mediaM, mediaG, mediaH];
+        await client.query(insertQuery, values);
+        client.release();
+        console.log("-----------------------------------------");
+        console.log(`Dados inseridos com sucesso!`);
+        console.log(`Nome: ${nome}, Idade: ${idade}, Série: ${serie}, Matematica: ${mediaM}, Geografia: ${mediaG}, História: ${mediaH}`);
+        console.log("-----------------------------------------");
+    }
+    catch (error) {
+        console.error("Ocorreu um erro ao interagir com o banco de dados:", error);
+    }
+    finally {
+        await pool.end();
+        console.log("Conexão com o banco de dados encerrada.");
+    }
+}
+inserirDados();
